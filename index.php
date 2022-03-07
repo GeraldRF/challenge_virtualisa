828a789c8728f1ba47a2d676ac2d98dfc399879d
Gerald Ramírez <geral.rafo@gmail.com>
2022-03-07 16:00:22 -0600

    Se agrego bootstrap descargado

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstap-css/bootstrap.min.css">
+    <script src="js/bootstrap-4.6.1-dist/js/bootstrap.bundle.min.js"></script>
+
     <title>Inicio</title>
 </head>
 
@@ -12,9 +15,10 @@
 
     <?php include "Views/components/navbar.php"; ?>
 
-    <div>
-        <h1>Bienvenidos</h1>
-        <h3>Administrador de empleados</h3>
+
+  <div class="container centered">
+        <h1 class="welcome">Bienvenidos</h1>
+        <h3 class="welcome">Administrador de empleados</h3>
     </div>
 
 
